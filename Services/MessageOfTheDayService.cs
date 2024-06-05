11c829ac547f6eaf1bf1a554c9343bfa8cad071b
Holly Plank <hollyplankdev@gmail.com>
2024-06-05 13:48:21 -0500

    Wrote MOTD service messages

--- Services/MessageOfTheDayService.cs
+++ Services/MessageOfTheDayService.cs
@@ -1,4 +1,5 @@
 using Microsoft.Extensions.Options;
+using MongoDB.Bson;
 using MongoDB.Driver;
 using MotdApiDotnet.Models;
 using MotdApiDotnet.Settings;
@@ -23,4 +24,33 @@ public class MessageOfTheDayService
         var mongoDatabase = mongoClient.GetDatabase(databaseSettings.Value.DatabaseName);
         motdCollection = mongoDatabase.GetCollection<MessageOfTheDayItem>(databaseSettings.Value.MotdCollectionName);
     }
+
+    //
+    //  Public Methods
+    //
+
+    public async Task CreateAsync(MessageOfTheDayItem newMotd) => await motdCollection.InsertOneAsync(newMotd);
+
+    public async Task<MessageOfTheDayItem?> GetLatestAsync() => await motdCollection.Find(_ => true).SortByDescending(motd => motd.CreatedAt).FirstOrDefaultAsync();
+
+    public async Task<MessageOfTheDayItem?> GetAsync(string id) => await motdCollection.Find(motd => motd.Id == id).FirstOrDefaultAsync();
+
+    public async Task<List<MessageOfTheDayItem>> ListPageAsync(int pageSize, string? previousLastId = null)
+    {
+        var query = motdCollection.Find(_ => true);
+
+        // If we have an ID to start from, filter our results to only be results before the given ID
+        if (previousLastId != null)
+        {
+            var filter = Builders<MessageOfTheDayItem>.Filter.Lt("_id", previousLastId);
+            query = motdCollection.Find(filter);
+        }
+
+        return await query.Limit(pageSize).ToListAsync();
+    }
+
+    public async Task UpdateAsync(string id, MessageOfTheDayItem updatedMotd) => await motdCollection.ReplaceOneAsync(motd => motd.Id == id, updatedMotd);
+
+    public async Task RemoveAsync(string id) => await motdCollection.DeleteOneAsync(motd => motd.Id == id);
+
 }
